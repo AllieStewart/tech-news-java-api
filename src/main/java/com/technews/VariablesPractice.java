a85cbab2e3e049a815ef1d8f5d663d72ba10f280
AllieStewart <megaxis1217@gmail.com>
2024-01-10 18:07:21 -0600

    Added some practice files in /src/main/java/com/technews

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int var1 = 43;
+        double var2 = 73.82;
+        System.out.println(var1 + var2);
+        // comes out as a double, "116.82"
+
+        String someWord = "idk";
+        System.out.println(var1 + someWord);
+        // comes out as "43idk"
+    }
+}
