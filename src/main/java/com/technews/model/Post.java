a542da76815142a94769c1698805b7f5ae8e3c82
AllieStewart <megaxis1217@gmail.com>
2024-01-11 16:37:15 -0600

    updated repository files with imports (Post), spaced out TechNewsJavaApiApplication code, edited /resources/application.properties to connect to MySQL

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 import java.util.Date;
 
